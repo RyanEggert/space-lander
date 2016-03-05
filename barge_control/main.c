033025d94b9a81eb4e3c486e6de5d6e3c2fcb500
RyanEggert <ryan.eggert@students.olin.edu>
2016-03-05 13:33:44 -0500

    Added basic dependencies and organizational structure Added lib Added folders for each PIC controller (independent so builds don't interfere). Added SConstruct files and main.c files Added skeleton structure to main.c files. Confirmed build success.

---
+++ barge_control/main.c
@@ -0,0 +1,14 @@
+#include <p24FJ128GB206.h>
+#include "config.h"
+#include "common.h"
+#include "uart.h"
+#include <stdio.h>
+
+int16_t main(void) {
+    init_clock();
+    init_uart();
+
+   while (1) {
+    }
+}
+
