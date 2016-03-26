ae8e94aafc8ed60da4666e693d62b7d7da789031
RyanEggert <ryan.eggert@students.olin.edu>
2016-03-26 15:46:24 -0400

    Added servo PWM writing functions to servo library.

--- lib/servo.c
+++ lib/servo.c
@@ -40,7 +40,8 @@ void servo_driver_wake(_SERVODRIVER *self) {
     servo_driver_end_transmission(self)
     __delay_us(500); // Oscillator takes max 500us to restart from sleep
     servo_driver_begin_transmission(self, I2C_WRITE);
-    servo_driver_write_register(PCA9685_MODE1, self -> mode1 | 0x81);
+    servo_driver_write_register(PCA9685_MODE1, self -> mode1 | 0xa1);
+    // 0xa1 ensures the raising of the restart bit, ALLCALL, and auto-increment
     servo_driver_end_transmission(self);
     // Servo driver is awake and functioning again!
 }
@@ -52,7 +53,11 @@ void servo_driver_reset(_SERVODRIVER *self) {
 }
 
 void servo_driver_set_pwm_freq(_SERVODRIVER *self, float new_freq) {
-    freq *= 0.9;  // Correct for overshoot in the frequency setting (see issue #11).
+    /*
+    Sets the PWM frequency (bounds 24Hz - 1526Hz). This involves sleeping and
+    restarting the servo driver.
+    */
+    freq *= 0.9;  // Correct for overshoot in the frequency setting
     float prescaleval = 25000000;
     prescaleval /= 4096;
     prescaleval /= freq;
@@ -66,10 +71,61 @@ void servo_driver_set_pwm_freq(_SERVODRIVER *self, float new_freq) {
     servo_driver_wake(self);
 }
 
+void servo_set_pwm(_SERVO *self, uint16_t on, uint16_t off) {
+
+    servo_driver_begin_transmission(self -> driver -> i2c_addr, I2C_WRITE);
+    servo_driver_write_register(self -> driver -> bus, LED0_ON_L + 4 * num);
+    servo_driver_write_register(self -> driver -> bus, on);
+    servo_driver_write_register(self -> driver -> bus, on >> 8);
+    servo_driver_write_register(self -> driver -> bus, off);
+    servo_driver_write_register(self -> driver -> bus, off >> 8);
+    servo_driver_end_transmission(self -> driver -> bus);
+}
+
+void servo_set(_SERVO *self, uint16_t val, bool invert) {
+    /*
+    Sets pin without having to deal with on/off tick placement and properly
+    handles a zero value as completely off.  Optional invert parameter supports
+    inverting the pulse for sinking to ground.
+    Val should be a value from 0 to 4095 inclusive.
+    */
+    // Clamp value between 0 and 4095 inclusive.
+    if ( val > 4095) {
+        val = 4095;
+    }
+    if (invert) {
+        if (val == 0) {
+            // Special value for signal fully on.
+            servo_set_pwm(self, 4096, 0);
+        }
+        else if (val == 4095) {
+            // Special value for signal fully off.
+            servo_set_pwm(self, 0, 4096);
+        }
+        else {
+            servo_set_pwm(self, 0, 4095 - val);
+        }
+    }
+    else {
+        if (val == 4095) {
+            // Special value for signal fully on.
+            servo_set_pwm(self, 4096, 0);
+        }
+        else if (val == 0) {
+            // Special value for signal fully off.
+            servo_set_pwm(self, 0, 4096);
+        }
+        else {
+            servo_set_pwm(self, 0, val);
+        }
+    }
+
+}
+
 
 void servo_driver_begin_transmission(_SERVODRIVER *dest, uint8_t rw) {
     /*
-    Addresses the specified servo driver over I2C. This should be followed by 
+    Addresses the specified servo driver over I2C. This should be followed by
     the writing of registers/other commands to this servo driver. rw specifies
     whether the following operations will be "read" (rw=1) or "write" (rw=0)
     operations.
