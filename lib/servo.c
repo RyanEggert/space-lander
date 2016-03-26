327314eeb67a5675fc73a22830104da9c4a416f6
RyanEggert <ryan.eggert@students.olin.edu>
2016-03-26 16:19:44 -0400

    Servo library.
    Creating init functions for the servo library.
    Additional documentation.
    Rewrote reset function to be in line with data sheet software reset protocol.
    Updated header to include functions.

--- lib/servo.c
+++ lib/servo.c
@@ -1,3 +1,10 @@
+/*
+** I2C SERVO LIBRARY
+** Adapted from Adafruit Arduino servo driver library
+** (https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library)
+**
+*/
+
 #include <p24FJ128GB206.h>
 #include <libpic30.h>
 #include "common.h"
@@ -5,12 +12,38 @@
 #include "i2c.h"
 #include "servo.h"
 
-void init_servo_driver(_SERVODRIVER *self, _I2C *bus, uint8_t hardware_addr) {
 
+_SERVODRIVER sd1;
+_SERVO orientation_servo, speed_ind_servo, fuel_ind_servo;
+
+
+void init_servo_driver(_SERVODRIVER *self, _I2C *bus, uint8_t hardware_addr) {
+    /*
+    Initializes an I2C servo driver device given an initialized _I2C object
+    and its 6-bit slave hardware address, as configured on the device.
+    */
+    self -> bus = bus;
+    self -> hardware_addr = 0x3F & (hardware_addr);
+    self -> i2c_addr = 0x40 | self -> hardware_addr; // Add preceding 1 to addr
+    self -> mode1 = 0b00100001; // Suggested mode configuration
+    i2c_open(self -> bus); // Open I2C bus
 }
 
-void init_servo(_SERVODRIVER *sd, uint8_t number) {
+void close_servo_driver_i2c(_SERVODRIVER *self) {
+    /*
+    Closes I2C communication object for the specified servo driver
+    */
+    i2c_close(self -> bus);
+}
 
+void init_servo(_SERVO *self, _SERVODRIVER *sd, uint8_t number) {
+    /*
+    Initializes a servo object connected to an I2C servo driver. Requires an
+    object representing the controlling I2C servo driver and the servo's
+    numerical address (0-16, incl.) on its controlling I2C servo driver.
+    */
+    self -> driver = sd;
+    self -> num = number;
 }
 
 
@@ -47,9 +80,13 @@ void servo_driver_wake(_SERVODRIVER *self) {
 }
 
 void servo_driver_reset(_SERVODRIVER *self) {
-    servo_driver_begin_transmission(self, I2C_WRITE);
-    servo_driver_write_register(self, PCA9685_MODE1, 0x0);
-    servo_driver_end_transmission(self);
+    /*
+    Follows the software reset procedure outlined in the datasheet.
+    */
+    i2c_start(self -> bus);
+    i2c_putc(self -> bus, 0x00);
+    i2c_putc(self -> bus, 0x06);
+    i2c_close(self -> bus);
 }
 
 void servo_driver_set_pwm_freq(_SERVODRIVER *self, float new_freq) {
@@ -72,9 +109,14 @@ void servo_driver_set_pwm_freq(_SERVODRIVER *self, float new_freq) {
 }
 
 void servo_set_pwm(_SERVO *self, uint16_t on, uint16_t off) {
+    /*
+    Sets a PWM signal as specified by the datasheet by writing 16-bit on and
+    "off" values into a series of four registers. This requires that auto-
+    increment be enabled in the MODE1 register.
+    */
 
     servo_driver_begin_transmission(self -> driver -> i2c_addr, I2C_WRITE);
-    servo_driver_write_register(self -> driver -> bus, LED0_ON_L + 4 * num);
+    servo_driver_write_register(self -> driver -> bus, DEV0_ON_L + 4 * num);
     servo_driver_write_register(self -> driver -> bus, on);
     servo_driver_write_register(self -> driver -> bus, on >> 8);
     servo_driver_write_register(self -> driver -> bus, off);
@@ -119,7 +161,6 @@ void servo_set(_SERVO *self, uint16_t val, bool invert) {
             servo_set_pwm(self, 0, val);
         }
     }
-
 }
 
 
@@ -145,6 +186,6 @@ uint8_t servo_driver_read_register(_SERVODRIVER *dest, uint8_t reg) {
     return i2c_getc(dest -> bus);
 }
 
-void servo_driver_end_transmission (_SERVODRIVER *dest) {
+void servo_driver_end_transmission(_SERVODRIVER *dest) {
     i2c_stop(dest -> bus);
 }
--- lib/servo.h
+++ lib/servo.h
@@ -1,5 +1,15 @@
+/*
+** I2C SERVO LIBRARY
+** Adapted from Adafruit Arduino servo driver library
+** (https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library)
+**
+*/
+
 #ifndef LIB_SERVO_H_
 #define LIB_SERVO_H_
+
+#include "common.h"
+#include "i2c.h"
 
 #define I2C_WRITE 0
 #define I2C_READ 1
@@ -11,17 +21,18 @@
 #define PCA9685_MODE1 0x0
 #define PCA9685_PRESCALE 0xFE
 
-#define LED0_ON_L 0x6
-#define LED0_ON_H 0x7
-#define LED0_OFF_L 0x8
-#define LED0_OFF_H 0x9
+#define DEV0_ON_L 0x6
+#define DEV0_ON_H 0x7
+#define DEV0_OFF_L 0x8
+#define DEV0_OFF_H 0x9
 
-#define ALLLED_ON_L 0xFA
-#define ALLLED_ON_H 0xFB
-#define ALLLED_OFF_L 0xFC
-#define ALLLED_OFF_H 0xFD
+#define ALLDEV_ON_L 0xFA
+#define ALLDEV_ON_H 0xFB
+#define ALLDEV_OFF_L 0xFC
+#define ALLDEV_OFF_H 0xFD
 
-
+extern _SERVODRIVER sd1;
+extern _SERVO orientation_servo, speed_ind_servo, fuel_ind_servo;
 
 // Servo Driver
 typedef struct {
@@ -29,16 +40,40 @@ typedef struct {
     uint8_t hardware_addr
     uint8_t i2c_addr;
     float i2c_freq;
-    uint8_t mode1; // Normal, awake mode of operation [MODE1 register]
-
+    uint8_t mode1;  // Normal, awake mode of operation [MODE1 register]
 } _SERVODRIVER;
 
 // Servo Driver
 typedef struct {
     _SERVODRIVER driver;
-    uint8_t num
-
+    uint8_t num;
 } _SERVO;
 
+
+void init_servo_driver(_SERVODRIVER *self, _I2C *bus, uint8_t hardware_addr);
+
+void close_servo_driver_i2c(_SERVODRIVER *self);
+
+void init_servo(_SERVO *self, _SERVODRIVER *sd, uint8_t number);
+
+void servo_driver_sleep(_SERVODRIVER *self);
+
+void servo_driver_wake(_SERVODRIVER *self);
+
+void servo_driver_reset(_SERVODRIVER *self);
+
+void servo_driver_set_pwm_freq(_SERVODRIVER *self, float new_freq);
+
+void servo_set_pwm(_SERVO *self, uint16_t on, uint16_t off);
+
+void servo_set(_SERVO *self, uint16_t val, bool invert);
+
+void servo_driver_begin_transmission(_SERVODRIVER *dest, uint8_t rw);
+
+void servo_driver_write_register(_SERVODRIVER *dest, uint8_t reg, uint8_t val);
+
+uint8_t servo_driver_read_register(_SERVODRIVER *dest, uint8_t reg);
+
+void servo_driver_end_transmission(_SERVODRIVER *dest);
 
 #endif  // LIB_SERVO_H_
