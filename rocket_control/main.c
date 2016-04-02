419251621e0c877bd0d4539cd95d3ab9cc5be9db
RyanEggert <ryan.eggert@students.olin.edu>
2016-04-02 13:25:47 -0400

    Continuing merge with master
    Pulling in master's version of rocket_control now that this branch's version has become tests/dcmotor_test.c (see previous commit)

--- rocket_control/main.c
+++ rocket_control/main.c
@@ -1,22 +1,20 @@
 #include <p24FJ128GB206.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include "config.h"
 #include "common.h"
 #include "pin.h"
 #include "ui.h"
 #include "timer.h"
 #include "uart.h"
-#include "oc.h"
-#include "md.h"
+#include "quad.h"
+#include "i2c.h"
+#include "servo.h"
 #include "main.h"
-
-_PIN *MOTOR_DIR;
-_PIN *FWD, *REV;
+#include <stdio.h>
+#include <stdlib.h>
 
 uint8_t MC_TXBUF[1024], MC_RXBUF[1024];
+uint8_t TX_BUF[1024], RX_BUF[1024];
+
 
 void print_buffer(uint8_t *buffer, uint16_t size) {
     int i;
@@ -88,119 +86,81 @@ void setup_uart() {
     //           0, MC_TXBUF, 1024, MC_RXBUF, 1024);
     // uart_open(&uart3, &AJTX, &AJRX, NULL, NULL, 115200., 'N', 1, 
     //           0, TXBUF, 1024, RXBUF, 1024);
-    pin_init(&AJRX, (uint16_t *)&PORTG, (uint16_t *)&TRISG, 
-             (uint16_t *)NULL, 6, -1, 8, 21, (uint16_t *)&RPOR10);
     pin_init(&AJTX, (uint16_t *)&PORTG, (uint16_t *)&TRISG, 
+             (uint16_t *)NULL, 6, -1, 8, 21, (uint16_t *)&RPOR10);
+    pin_init(&AJRX, (uint16_t *)&PORTG, (uint16_t *)&TRISG, 
              (uint16_t *)NULL, 7, -1, 0, 26, (uint16_t *)&RPOR13);
     uart_open(&uart1, &AJTX, &AJRX, NULL, NULL, 19200., 'N', 1, 
-              0, MC_TXBUF, 1024, MC_RXBUF, 1024);
+             0, TX_BUF, 1024, RX_BUF, 1024);
+    _UART *_stdout, *_stderr;
+    _stdout = &uart1;
+    _stderr = &uart1;
 }
 
 void setup() {
+    printf("START\n\r");
     timer_setPeriod(&timer1, 1);  // Timer for LED operation/status blink
-    // timer_setPeriod(&timer2, 0.5);
-    timer_setPeriod(&timer3, .01);
+    timer_setPeriod(&timer2, 0.5);
+    timer_setPeriod(&timer3, 0.01);
     timer_start(&timer1);
-    // timer_start(&timer2);
+    timer_start(&timer2);
     timer_start(&timer3);
-    // setup_uart();
+
+    quad_init(&quad1, &D[12], &D[13]); // quad1 uses pins D12 & D13
+    quad_every(&quad1, &timer5, 0.0000875); // quad1 will use timer5 interrupts
+
+    // Optional Quad Encoder Debug Pins
+    pin_digitalOut(&D[6]); // b0
+    pin_digitalOut(&D[7]); // b1
+    pin_digitalOut(&D[8]); // b2
+    pin_digitalOut(&D[9]); // b3
+
+    // Debug button output pins
+    pin_digitalOut(&D[11]);
+    pin_digitalOut(&D[10]);
+
+    // General use debugging output pin
+    pin_digitalOut(&D[2]);
+
+    setup_uart();
 }
 
 int16_t main(void) {
-        // printf("Starting Rocket Controller...\r\n");
+    // printf("Starting Rocket Controller...\r\n");
     init_clock();
     init_ui();
     init_timer();
     init_uart();
-    init_pin();
-    init_oc();
-    // ***init_md:  may need to change md.c so it utilizes less DIO pins    
-    init_md();
+    init_quad();
     setup();
-
     uint16_t counter = 0;
     uint8_t rec_msg [64];
     uint64_t msg;
     char is_recip = 0;
-
-    // Initialize motor vars/pins
-    uint16_t MOTOR_SPEED = 0xC000;
-    uint16_t MOTOR_DIR_TRACK = 0;   // tracks vert dir of rocket
-    MOTOR_DIR = &D[0];
-    FWD = &D[2];
-    REV = &D[13];
-    pin_digitalOut(MOTOR_DIR);
-
-    uint8_t direction = 1;
-
-    const uint16_t MOTOR_SPEED_LIMIT = 0xFFF0;
-    const uint16_t GRAV_VAL = 0x00F0;
-    const uint16_t THRUST_VAL = GRAV_VAL*2;
-    uint16_t mot_speed = 0;
-    int mot_dir = 0;
-    pin_clear(MOTOR_DIR);
     while (1) {
-
+        if (timer_flag(&timer1)) {
+            // Blink green light to show normal operation.
+            timer_lower(&timer1);
+            led_toggle(&led2);
+        }
         if (timer_flag(&timer2)) {
             timer_lower(&timer2);
-            uart_gets(&uart1, rec_msg, 64);
-            msg = convert_msg(rec_msg);
-            is_recip = parse_addr(&msg);
-            if (is_recip == 1) {
-                led_on(&led3);
+            if (quad1.count % 2 == 0) {
+                led_on(&led1);
             } else {
-                led_off(&led3);
+                led_off(&led1);
             }
         }
-        if(timer_flag(&timer3)) {
+        if (timer_flag(&timer3)) {
             timer_lower(&timer3);
-            // Handle rocket thrust
-            if (pin_read(FWD)) { // Thrust on
-                if (MOTOR_DIR_TRACK == 0) {  // rocket falling
-                    if (MOTOR_SPEED > THRUST_VAL) {  // nonzero velocity
-                        MOTOR_SPEED = MOTOR_SPEED - THRUST_VAL;
-                    }
-                    else {  // zero velocity
-                        MOTOR_DIR_TRACK = 1;
-                        MOTOR_SPEED = 0;
-                    }
-                }
-                else { // rocket rising
-                    if (MOTOR_SPEED < MOTOR_SPEED_LIMIT) {
-                        MOTOR_SPEED = MOTOR_SPEED + THRUST_VAL;
-                    }
-                }
-                // pin_set(MOTOR_DIR);
-                // if (MOTOR_SPEED < MOTOR_SPEED_LIMIT) {
-                //     MOTOR_SPEED = MOTOR_SPEED + THRUST_VAL;
-                // }
-                // md_speed(&mdp, MOTOR_SPEED);
-                led_on(&led2);
-            }
-            else { // no thrust
-                if (MOTOR_DIR_TRACK == 0) { // rocket falling
-                    if (MOTOR_SPEED < MOTOR_SPEED_LIMIT) {
-                        MOTOR_SPEED = MOTOR_SPEED + GRAV_VAL;
-                    }
-                }
-                else {  // rocket rising
-                    if (MOTOR_SPEED > THRUST_VAL) {
-                        MOTOR_SPEED = MOTOR_SPEED - GRAV_VAL;
-                    }
-                    else {
-                        MOTOR_DIR_TRACK = 0;
-                        MOTOR_SPEED = GRAV_VAL;
-                    }
-                }
-                led_off(&led2);
-            }
+            uint16_t switch2 = !sw_read(&sw2);
+            uint16_t switch3 = !sw_read(&sw3);
+            // led_write(&led1, switch2);
+            led_write(&led3, switch3);
+
+            pin_write(&D[10], switch2);
+            pin_write(&D[11], switch3);
+
         }
-        if (MOTOR_DIR_TRACK) {
-            pin_set(MOTOR_DIR);
-        }
-        else {
-            pin_clear(MOTOR_DIR);
-        }
-        md_speed(&mdp, MOTOR_SPEED);
     }
 }
