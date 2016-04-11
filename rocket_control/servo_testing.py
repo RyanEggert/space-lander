fb1e1f63e13ccd6de491d1edc11a641fe7eae3b3
RyanEggert <ryan.eggert@students.olin.edu>
2016-04-11 19:06:49 -0400

    Characterizing servos

--- rocket_control/servo_testing.py
+++ rocket_control/servo_testing.py
@@ -1,9 +1,18 @@
 from usb_vendor import PIC_USB
-
+import time
 def main():
     c = PIC_USB(0x0005)
-    c.debug_servo_set_pos(5, 2500)
-    # c.debug_servo_set_freq(50)
+    c.debug_servo_set_freq(50)
+    c.debug_servo_set_pos(7, 120)
+    time.sleep(1)
+    # c.debug_servo_set_pos(8, 450)
+
+    for pos in range(120, 450, 3):
+        print pos
+        c.debug_servo_set_pos(7, pos)
+
+        c.debug_servo_set_pos(8, pos)
+        time.sleep(.05)
 
 if __name__ == '__main__':
     # import argparse
