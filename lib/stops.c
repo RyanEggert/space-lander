4db7645d6976b58bc770037da5101f53c4c564d4
RyanEggert <ryan.eggert@students.olin.edu>
2016-04-16 20:36:27 -0400

    Documenting tri-state stop read return

--- lib/stops.c
+++ lib/stops.c
@@ -34,10 +34,13 @@ void init_stops(void) {
 bool stop_read(_ESTOP *self) {
     /*
     Reads endstop with debouncing. This should be called at a fixed time
-    interval (e.g., with timer interrupts).
+    interval (e.g., with timer interrupts). Returns a tri-state boolean
+    (uint8_t) value representing observed change. Returns 0 (false) if button has 
+    transitioned from high to low, 1 (true) if button has transitioned from low
+    to high, 2 (third state) if no state change has occurred.
     */
     self->state = (self->state << 1) | pin_read(self->pin) | 0xe000;
-    uint8_t ret = 2;
+    uint8_t ret = 2;  // Retunrs 2 if no state change.
     if (self->state == 0xf000) {
         self->hit = true;
         ret = true;
