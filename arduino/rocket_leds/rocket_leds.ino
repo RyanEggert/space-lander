495e43bdaf727ea0a7bdb4fc148887a1004778a6
awilkinson88 <kasja525@gmail.com>
2016-04-23 13:24:00 -0400

    basic led code working. To do: add throttle

--- arduino/rocket_leds/rocket_leds.ino
+++ arduino/rocket_leds/rocket_leds.ino
@@ -6,59 +6,82 @@
 #define LOSE 0b10
 #define WIN 0b11
 
-#define LED_PIN 6
-#define ST_PIN1 1
-#define ST_PIN2 2
+#define BARGE_PIN 6
+#define LIVES_PIN 7
+#define FIRE_PIN 8
+
+#define ST_PIN1 2
+#define ST_PIN2 3
 
 typedef void (*STATE_HANDLER_T)(void);
 STATE_HANDLER_T state, last_state;
+
 void win(void);
 void lose(void);
 void flying(void);
 void idle(void);
 
-
 volatile int state_var = 0b00;
+int lives = 3;
 
-
-Adafruit_NeoPixel barge_strip = Adafruit_NeoPixel(60, LED_PIN, NEO_GRB + NEO_KHZ800);
-//Adafruit_NeoPixel lives_strip = Adafruit_NeoPixel(3, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel barge_strip = Adafruit_NeoPixel(4, BARGE_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel lives_strip = Adafruit_NeoPixel(3, LIVES_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel fire_strip = Adafruit_NeoPixel(1, FIRE_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void win(){
-   for(int i=0;i<4;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    barge_strip.setPixelColor(i, barge_strip.Color(0,255,0)); // Moderately bright green color.
-    barge_strip.show(); // This sends the updated pixel color to the hardware.
-  }  
+   if (state != last_state) {
+     for(int i=0;i<4;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      barge_strip.setPixelColor(i, barge_strip.Color(0,255,0)); // bright green color.
+      barge_strip.show(); // This sends the updated pixel color to the hardware.
+      }
+   }
 };
 
 void lose(){
-    for(int i=0;i<4;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    barge_strip.setPixelColor(i, barge_strip.Color(255,0,0)); // Moderately bright red color.
-    barge_strip.show(); // This sends the updated pixel color to the hardware.
-  }  
+   if (state != last_state) {
+     for(int i=0;i<4;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      barge_strip.setPixelColor(i, barge_strip.Color(255,0,0)); // Moderately bright red color.
+      barge_strip.show(); // This sends the updated pixel color to the hardware.
+      }
+     lives --;
+     lives_strip.setPixelColor(lives, 0); // Turn it off
+     lives_strip.show(); // This sends the updated pixel color to the hardware.
+      }  
 };
 
 void flying(){
-  for(int i=0;i<4;i++){
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    barge_strip.setPixelColor(i, barge_strip.Color(255,255,255)); // white
-    barge_strip.show(); // This sends the updated pixel color to the hardware.
+  if (state != last_state) {
+    for(int i=0;i<4;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      barge_strip.setPixelColor(i, barge_strip.Color(255,255,255)); // white
+      barge_strip.show(); // This sends the updated pixel color to the hardware.
+    }
+    fire_strip.setPixelColor(0, fire_strip.Color(255,72,0));
+    fire_strip.show();
   }   
 };
 
 void idle(){
-  for(int i=0;i<4;i++){
+  if (state != last_state) {
+    for(int i=0;i<4;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      barge_strip.setPixelColor(i, barge_strip.Color(255,255,255)); // white
+      barge_strip.show(); // This sends the updated pixel color to the hardware.
+    }
+    lives = 3;
+    for(int i=0;i<3;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    barge_strip.setPixelColor(i, barge_strip.Color(255,255,255)); // white
-    barge_strip.show(); // This sends the updated pixel color to the hardware.
+    lives_strip.setPixelColor(i, barge_strip.Color(0,255,0)); // bright green color.
+    lives_strip.show(); // This sends the updated pixel color to the hardware.
+  }  
   }  
 };
 
 void setup(){
-  Timer1.initialize(10000); //toggles every .01 seconds
+  Timer1.initialize(10000); //toggles every .1 seconds
   Timer1.attachInterrupt(callback); //attaches as timer overflow interrupt
   pinMode(ST_PIN1, INPUT); //state_var pin 1
   pinMode(ST_PIN2, INPUT); //sate pin 2  
@@ -66,18 +89,25 @@ void setup(){
   barge_strip.begin();
   barge_strip.show();
   
+  lives_strip.begin();
+  lives_strip.show();
+  
   state = idle;
   last_state = (STATE_HANDLER_T)NULL;
   
   Serial.begin(9600);
   
-  //lives_strip.begin();
-  //lives_strip.show();
+  lives_strip.begin();
+  lives_strip.show();
+  
+  fire_strip.begin();
+  fire_strip.show();
 }
 
 void callback()
 {
   state_var = 0b00;
+  last_state = state;
   state_var = (state_var | digitalRead(ST_PIN2) | (digitalRead(ST_PIN1) << 1));
   Serial.println(state_var, BIN);
   switch(state_var){
